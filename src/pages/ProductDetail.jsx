ba964ba237f7d49a01c3a9e5e7f58b26a9137339
cagatay.yildiz <cagatay@pixupplay.com>
2024-09-10 10:15:07 +0300

    products and product detail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { productsData } from '../data/productsData'
 
 function ProductDetail() {
 
     const { id } = useParams()
 
+    const navigate = useNavigate()
+
     let product = productsData.find(item => item.id == id)
 
-    if(!product)
+    if (!product)
         return <h1>Product not found!</h1>
 
     return <>
+
+        <button onClick={() => navigate(-1)}>Go to back!</button>
 
         <h2>Id: {id}</h2>
         <hr />
@@ -22,7 +26,7 @@ function ProductDetail() {
         <h2>Stock: {product.unitsInStock}</h2>
         <hr />
         <h2>Quantity Per Unit: {product.quantityPerUnit}</h2>
-    
+
     </>
 }
 
